29123e07423df3e125fad306bfa21efdff739317
Ronald Davis <ronnied1222@msn.com>
2018-05-13 16:21:04 -0400

    add functions

---
+++ Lambda/homeloss.js
@@ -0,0 +1,28 @@
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+exports.handler = function(e, ctx, callback){
+    var params = {
+            ProjectionExpression: "#winner",
+            FilterExpression: '#winner = :team and #home = :team',
+            TableName: "MLBGames",
+            ExpressionAttributeNames: {
+                "#winner" : "Losing Team",
+                "#home": "Home Team"
+            },
+            ExpressionAttributeValues: {
+                ':team' : e.teamName
+            }
+        };
+    console.log(e);
+   // callback(null, e.gameID);
+    
+    docClient.scan(params, function(err,data){
+        if(err){
+            callback(err,null);
+        }else{
+            callback(null, data["Count"]);
+        }
+    });
+}
---
+++ Lambda/runWinsgreater.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+exports.handler = function(e, ctx, callback){
+    var params = {
+            ProjectionExpression: "#winner",
+            FilterExpression: '#winner = :team and #away = :team and #awayRuns > :runs or #winner = :team and #home = :team and #homeRuns > :runs',
+            TableName: "MLBGames",
+            ExpressionAttributeNames: {
+                "#winner" : "Winning Team",
+                "#away" : "Away Team",
+                "#awayRuns" : "Away Runs",
+                "#home" : "Home Team",
+                "#homeRuns" : "Home Score"
+                
+            },
+            ExpressionAttributeValues: {
+                ':team' : e.teamName,
+                ':runs' : Number(e.runs)
+            }
+        };
+    console.log(e);
+   // callback(null, e.gameID);
+    
+    docClient.scan(params, function(err,data){
+        if(err){
+            callback(err,null);
+        }else{
+            callback(null, data["Count"]);
+        }
+    });
+}
